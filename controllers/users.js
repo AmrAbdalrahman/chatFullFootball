603457d11f1d654d500ec56626197ef0fd54f842
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-01-20 19:22:46 +0200

    login with google and facebook

--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,34 @@
 'use strict';
 
-module.exports = function (_, passport,User) {
+module.exports = function (_, passport, User) {
 
     return {
 
         SetRouting: function (router) {
             router.get('/', this.indexPage);
             router.get('/signup', this.getSignUp);
+
+            router.get('/home', this.homePage);
+            router.get('/auth/facebook', this.getFacebookLogin);
+            router.get('/auth/facebook/callback', this.facebookLogin);
+            router.get('/auth/google', this.getGoogleLogin);
+            router.get('/auth/google/callback', this.googleLogin);
+
+
             //login
-            router.post('/', User.LoginValidation,this.postLogin);
-            router.post('/signup',User.SignUpValidation ,this.postSignUp);
-            router.get('/home',this.homePage);
+            router.post('/', User.LoginValidation, this.postLogin);
+            router.post('/signup', User.SignUpValidation, this.postSignUp);
+
+
+            //logout
+            router.get('/logout', this.userLogout);
 
 
         },
 
         indexPage: function (req, res) {
             const errors = req.flash('error');
-            return res.render('index',{title: 'Footballkk | login',messages: errors,hasErrors: errors.length > 0})
+            return res.render('index', {title: 'Footballkk | login', messages: errors, hasErrors: errors.length > 0})
         },
 
         postLogin: passport.authenticate('local.login', {
@@ -30,7 +41,7 @@ module.exports = function (_, passport,User) {
 
         getSignUp: function (req, res) {
             const errors = req.flash('error');
-            return res.render('signup',{title: 'Footballkk | SignUp',messages: errors,hasErrors: errors.length > 0});
+            return res.render('signup', {title: 'Footballkk | SignUp', messages: errors, hasErrors: errors.length > 0});
         },
 
         postSignUp: passport.authenticate('local.signup', {
@@ -40,10 +51,44 @@ module.exports = function (_, passport,User) {
 
         }),
 
+        getFacebookLogin: passport.authenticate('facebook', {
+            scope: 'email'
+        }),
+
+        facebookLogin: passport.authenticate('facebook', {
+            successRedirect: '/home',
+            failureRedirect: '/signup',
+            failureFlash: true
+        }),
+
+        getGoogleLogin: passport.authenticate('google', {
+            scope: ['https://www.googleapis.com/auth/plus.login',
+                    'https://www.googleapis.com/auth/plus.profile.emails.read']
+        }),
+
+        googleLogin: passport.authenticate('google', {
+            successRedirect: '/home',
+            failureRedirect: '/signup',
+            failureFlash: true
+
+        }),
+
+        userLogout: function (req, res) {
+            res.clearCookie("key");
+            res.redirect('/');
+            /*cookie = req.cookies;
+            for (let prop in cookie) {
+                if (!cookie.hasOwnProperty(prop)) {
+                    continue;
+                }
+                res.cookie(prop, '', {expires: new Date(0)});
+            }
+            res.redirect('/')*/
+        },
 
 
         homePage: function (req, res) {
             return res.render('home');
         }
-}
+    }
 }
---
+++ secret/secretFile.js
@@ -0,0 +1,11 @@
+module.exports = {
+    facebook: {
+        clientID: '234179700844362',
+        clientSecret: '5397948f6399696f798ad898c0ab1857',
+    },
+
+    google: {
+        clientID: '338396516084-24bdcnjo4sm2hgiu8t3rqs7oj4js13q0.apps.googleusercontent.com',
+        clientSecret: 'qf8boUvVE9ZaayWsM6bxCEdi',
+    }
+}

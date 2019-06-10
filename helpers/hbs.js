24ecfa936f3de94b856ba01b59da70ebc393eb89
AmrAlmagic <amr.abdalrahman94@gmail.com>
2019-06-10 22:19:36 +0200

    count messages in navbar

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -3,6 +3,18 @@ const express_handlebars_sections = require('express-handlebars-sections');
 let register = function (Handlebars) {
     let helpers = {
         section: express_handlebars_sections(),
+        inc: function (value, options) {
+             parseInt(value) + 1;
+        },
+        setVar: function(varName, varValue, options) {
+            options.data.root[varName] = parseInt(varValue);
+        },
+        ifCond : function(v1, v2, options) {
+            if(v1 === v2) {
+                return options.fn(this);
+            }
+            return options.inverse(this);
+        }
     };
 
     if (Handlebars && typeof Handlebars.registerHelper === "function") {
